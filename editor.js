2a14d97aaef7f5674f8e32b3072c89945c48dab2
Anasomari93 <84619600+Anasomari93@users.noreply.github.com>
2022-07-16 09:08:04 +0300

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,78 @@
+var english=document.getElementById("english");
+var arabic=document.getElementById("arabic");
+var spanish=document.getElementById("spanish");
+var french=document.getElementById("french");
+var title=document.getElementById("title");
+// photo editor
+var text=document.getElementById("text");
+var install=document.getElementById("install");
+var rate= document.getElementById("rate");
+var main= document.getElementById("main");
+// photo motion
+var title1=document.getElementById("title1");
+var text1=document.getElementById("text1");
+var install1=document.getElementById("install1");
+var rate1= document.getElementById("rate1");
+var main1= document.getElementById("main1");
+
+
+
+
+english.onclick = ()=>{
+    main.style.textAlign="left"
+    title.innerHTML="Photo Editor & Effects : Neon";
+    text.innerHTML="This app contain amazing Effects : Neon Effects , Wings Effects ,  Drip Effects , Motion Effects , Black and white Effects , Blur Effects , Pix Lab Effects with Filters and Frames for your photos ."
+    rate.innerHTML="*If you enjoyed this app, would you mind taking a moment to rate it? It won't take more than a minute. Thanks for your support ."
+    install.innerHTML="Click to Install";
+    //photo motion
+    main1.style.textAlign="left"
+    title1.innerHTML="Animation Maker – wallpapers";
+    text1.innerHTML="Make your photos a live wallpaper with simple tools and amazing results. This application features a lot of great tools to make things easy. Try it now."
+    rate1.innerHTML="*If you enjoyed this app, would you mind taking a moment to rate it? It won't take more than a minute. Thanks for your support ."
+    install1.innerHTML="Click to Install";
+
+}
+arabic.onclick=()=>{
+    main.style.textAlign="right"
+    title.innerHTML="محرر الصور & تأثيرات : نيون";
+    text.innerHTML="يحتوي هذا التطبيق على تأثيرات مذهلة: تأثيرات النيون ، تأثيرات الأجنحة ، تأثيرات التنقيط ، تأثيرات الحركة ، التأثيرات بالأبيض والأسود ، تأثيرات التمويه والكثير من الفلاتر والإطارات لصورك."
+    rate.innerHTML=". إذا استمتعت بهذا التطبيق ، فهل تمانع في قضاء بعض الوقت لتقييمه؟ لن يستغرق الأمر أكثر من دقيقة. شكرا لدعمك *"
+    install.innerHTML="التحميل من هنا";
+     //photo motion
+     main1.style.textAlign="right"
+     title1.innerHTML="صانع الخلفيات – خلفيات متحركة";
+     text1.innerHTML="اصنع من صورك خلفية حية بادوات بسيطة ونتائج مذهلة يتميز هذا التطبيق بالكثير من الادوات الرائعة لتجعل الامور سهلة جربها الان."
+     rate1.innerHTML=". إذا استمتعت بهذا التطبيق ، فهل تمانع في قضاء بعض الوقت لتقييمه؟ لن يستغرق الأمر أكثر من دقيقة. شكرا لدعمك *"
+     install1.innerHTML="التحميل من هنا";
+
+}
+
+spanish.onclick=()=>{
+    main.style.textAlign="left"
+    title.innerHTML="Editor de fotos & Efectos :Neón";
+    text.innerHTML="Convierta sus fotos en un fondo de pantalla en vivo con herramientas simples y resultados sorprendentes. Esta aplicación presenta muchas herramientas excelentes para facilitar las cosas. Pruebalo ahora."
+    rate.innerHTML="*Si disfrutó de esta aplicación, ¿le importaría tomarse un momento para calificarla? No tardará más de un minuto. Gracias por su apoyo ."
+    install.innerHTML="Haga clic para instalar";
+     //photo motion
+     main1.style.textAlign="left"
+     title1.innerHTML="fondo de pantalla de animación";
+     text1.innerHTML="Convierta sus fotos en un fondo de pantalla en vivo con herramientas simples y resultados sorprendentes. Esta aplicación presenta muchas herramientas excelentes para facilitar las cosas. Pruebalo ahora."
+     rate1.innerHTML="*Si disfrutó de esta aplicación, ¿le importaría tomarse un momento para calificarla? No tardará más de un minuto. Gracias por su apoyo ."
+     install1.innerHTML="Haga clic para instalar";
+    
+}
+
+french.onclick=()=>{
+    main.style.textAlign="left"
+    title.innerHTML="Éditeur photos & Effets : Néon";
+    text.innerHTML="Cette application contient des effets étonnants: effets néon, effets d'ailes, effets goutte à goutte, effets de mouvement, effets noir et blanc, effets de flou, effets Pix Lab avec filtres et cadres pour vos photos."
+    rate.innerHTML="* Si vous avez apprécié cette application, pourriez-vous prendre un moment pour l'évaluer ? Cela ne prendra pas plus d'une minute. Merci pour votre aide ."
+    install.innerHTML="Cliquez pour installer";
+     //photo motion
+     main1.style.textAlign="left"
+     title1.innerHTML="Animation Maker  fonds d'écran";
+     text1.innerHTML="Faites de vos photos un fond d'écran animé avec des outils simples et des résultats étonnants. Cette application propose de nombreux outils formidables pour faciliter les choses. Essayez-le maintenant."
+     rate1.innerHTML="* Si vous avez apprécié cette application, pourriez-vous prendre un moment pour l'évaluer ? Cela ne prendra pas plus d'une minute. Merci pour votre aide ."
+     install1.innerHTML="Cliquez pour installer";
+    
+}
